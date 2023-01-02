518627fa409eaaf4f08c507ac44ef55bff944cde
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-02 13:29:14 +0100

    pages

--- next-js-tutorial/pages/index.js
+++ next-js-tutorial/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/Link'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
@@ -60,19 +61,9 @@ export default function Home() {
         </div>
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+          <Link href='/posts/Post' >
+            <h2>Create your blog post</h2>
+          </Link>
 
           <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
