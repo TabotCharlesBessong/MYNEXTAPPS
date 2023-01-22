49f086809bac3006264d18f9f969ce13307a73ce
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-22 08:29:20 +0100

    let do some authentication

---
+++ googleAuth/pages/_app.js
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import { QueryClientProvider,QueryClient } from 'react-query'
+import {store} from '../redux/store'
+import {Provider} from 'react-redux'
+
+//create client 
+const queryClient = new QueryClient()
+
+export default function App({ Component, pageProps }) {
+  return (
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<Component {...pageProps} />
+			</Provider>
+		</QueryClientProvider>
+	);
+}
---
+++ googleAuth/redux/store.js
@@ -0,0 +1,9 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import Reducer from './reducer'
+
+export const store = configureStore({
+  reducer:{
+    app:Reducer
+  }
+})
