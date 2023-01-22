e3ccbebb63b1940b6bec8c6c390b609cf0b05058
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-22 10:49:18 +0100

    layout created

---
+++ googleAuth/pages/login.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Layout from '../layout/Layout'
+
+const Login = () => {
+  return (
+		<Layout>
+			<Head>
+				<title>Login</title>
+			</Head>
+			<h1>Hello</h1>
+		</Layout>
+	);
+}
+
+export default Login
---
+++ googleAuth/pages/register.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Head from 'next/head'
+import Layout from '../layout/Layout'
+
+const register = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <h1>Register</h1>
+    </Layout>
+  )
+}
+
+export default register
