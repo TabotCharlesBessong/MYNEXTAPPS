884e36b54e6f14ccf6ee8de1aa18c2e1eb9c01bf
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-23 14:30:41 +0100

    fixed the bug and things are ok now

--- googleAuth/model/Schema.js
+++ googleAuth/model/Schema.js
@@ -1,7 +1,7 @@
 import { Schema,model,models } from "mongoose";
 
 const userSchema = new Schema({
-  name:String,
+  username:String,
   email:String,
   password:String
 })
--- googleAuth/pages/api/auth/signup.js
+++ googleAuth/pages/api/auth/signup.js
@@ -1,19 +1,20 @@
-
 import connectMongo from "../../../database/connection"
 import Users from "../../../model/Schema"
 import {hash} from 'bcrypt'
 
-export default async function handler(req,res){
-  connectMongo().catch(error => res.json({error:'Connection Failed...!'}))
+export default async function handler(req, res, next) {
+  connectMongo().catch(error => res.json({error:'Connection failed'}))
 
+  // only post method is accepted
   if(req.method === 'POST'){
-    if(!req.body) return res.status(404).json({error:'Dont"t Have form data'})
+    if(!req.body) return res.status(404).json({error:'Does not have form data'})
+
     const {username,email,password} = req.body
 
-    // check duplicate users
+    // check duplicate user
     const existing = await Users.findOne({email})
-    if(existing) return res.status(422).json({message:'User already exist'})
-
+    if(existing) return res.status(422).json({message:"User already exist...!"})
+    
     // hash password
     Users.create({username,email,password:await hash(password,12)},function(err,data){
       if(err) return res.status(404).json({err})
@@ -21,6 +22,6 @@ export default async function handler(req,res){
     })
 
   }else{
-    res.status(500).json({message:'HTTP method not valid only POST method accepted'})
+    res.status(500).json({message:'HTTP Method not valid , only post method accepted'})
   }
 }
