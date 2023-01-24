fc374a8585434e40d4e208fa8fe628274c769011
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-24 12:35:53 +0100

    fixing security issues with gitguardians

--- RealTor/utils/fetchApi.js
+++ RealTor/utils/fetchApi.js
@@ -6,7 +6,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
   const {data} = await axios.get(url, {
 		headers: {
-			"X-RapidAPI-Key": "aa90f9069dmsh77129c141cac057p10e255jsn3b7b6110ee58",
+			"X-RapidAPI-Key": process.env.RAPID_API_KEY,
 			"X-RapidAPI-Host": "bayut.p.rapidapi.com",
 		},
 	});
