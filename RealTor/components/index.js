d3e409453fd4886dbe70634f13eb510d0de9ab0a
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-24 10:17:13 +0100

    ignoring some files for now

--- RealTor/components/index.js
+++ RealTor/components/index.js
@@ -1,7 +1,7 @@
 
 export {default as Property} from './Property'
 export {default as Footer} from './Footer'
-export {default as ImageScrollbar} from './ImageScrollbar'
+// export {default as ImageScrollbar} from './ImageScrollbar'
 export {default as Layout} from './Layout'
 export {default as Navbar} from './Navbar'
-export {default as SearchFilters} from './SearchFilters'
+// export {default as SearchFilters} from './SearchFilters'
