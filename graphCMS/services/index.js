d9b8c0a642926cdb1117ceae881c706606666eda
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-30 12:04:29 +0100

    from 3 to 5

--- graphCMS/services/index.js
+++ graphCMS/services/index.js
@@ -93,7 +93,7 @@ export const getSimilarPosts = async (categories, slug) => {
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
         where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
-        last: 3
+        last: 5
       ) {
         title
         featuredImage {
@@ -239,7 +239,7 @@ export const getRecentPosts = async () => {
     query GetPostDetails() {
       posts(
         orderBy: createdAt_ASC
-        last: 3
+        last: 5
       ) {
         title
         featuredImage {
