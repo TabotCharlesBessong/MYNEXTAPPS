f406b994da53494abd8d623b1d06056c80c47309
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-30 12:32:02 +0100

    adjustments

--- graphCMS/components/Author.jsx
+++ graphCMS/components/Author.jsx
@@ -10,8 +10,8 @@ const Author = ({ author }) => (
         unoptimized
         loader={grpahCMSImageLoader}
         alt={author.name}
-        height="100px"
-        width="100px"
+        height={100}
+        width={100}
         className="align-middle rounded-full"
         src={author.photo.url}
       />
