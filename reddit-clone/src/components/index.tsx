d5dc71604671d87a13c0b1cfe199e9d135df3651
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-02 20:05:58 +0100

    setting up layout

---
+++ reddit-clone/src/components/index.tsx
@@ -0,0 +1,2 @@
+export {default as Layout} from './layout/Layout'
+export {default as Navbar} from './navbar/Navbar'
--- reddit-clone/src/pages/_app.tsx
+++ reddit-clone/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app'
 import {ChakraProvider} from '@chakra-ui/react'
+import {theme} from '../chakra/theme'
+import { Layout } from '../components'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme} > 
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
