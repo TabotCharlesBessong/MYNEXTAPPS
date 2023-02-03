cbbc99bf4b8a8bddc3d3f6107c78fbb6d25fa939
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-03 14:14:36 +0100

    firebase authentication with google and github

--- reddit-clone/src/components/modal/auth/OAuthButtons.tsx
+++ reddit-clone/src/components/modal/auth/OAuthButtons.tsx
@@ -1,17 +1,36 @@
 import { Button, Flex, Image, Text } from '@chakra-ui/react'
+import {useSignInWithGoogle,useSignInWithGithub} from 'react-firebase-hooks/auth'
+import { auth } from '@/src/firebase/clientApp'
 import React from 'react'
 
 const OAuthButtons:React.FC = () => {
+
+  const [
+    signInWithGoogle,
+    user,
+    loading,
+    userError
+  ] = useSignInWithGoogle(auth)
+
+  const [
+    signInWithGithub,
+    userGithub,
+    loadingGithub,
+    userErrorGithub
+  ] = useSignInWithGithub(auth)
   return (
     <Flex direction='column' width='100%' mb={4} >
-      <Button variant='oauth' mb={3} display='flex' justifyContent='space-evenly' alignItems='center' >
+      <Button isLoading={loading} onClick={() => signInWithGoogle()} variant='oauth' mb={3} display='flex' justifyContent='space-evenly' alignItems='center' >
         <Image mr={4} width={30} height={30} src='/images/googlelogo.png' alt='' />
         <Text>Continue With Google</Text>
       </Button>
-      <Button variant='oauth' mb={3} display='flex' justifyContent='space-evenly' alignItems='center' >
+      <Button isLoading={loadingGithub} onClick={() => signInWithGithub()}  variant='oauth' mb={3} display='flex' justifyContent='space-evenly' alignItems='center' >
         <Image mr={4} width={30} height={30} src='/images/githubLogo.png' alt='' />
         <Text>Continue With Github</Text>
       </Button>
+
+      {userError && <Text>{userError.message}</Text>}
+      {userErrorGithub && <Text>{userErrorGithub.message}</Text>}
     </Flex>
   )
 }
