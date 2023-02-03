aef9f4d90b277687a4fc0eea6602c0ee88a9f053
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-03 14:15:33 +0100

    login

--- reddit-clone/src/components/modal/auth/Login.tsx
+++ reddit-clone/src/components/modal/auth/Login.tsx
@@ -2,6 +2,9 @@ import React,{useState} from 'react'
 import {Input,Button,Flex,Text} from '@chakra-ui/react'
 import {useSetRecoilState} from 'recoil'
 import { authModalState } from '@/src/atoms/authModalAtom'
+import { auth } from '@/src/firebase/clientApp'
+import { FIREBASE_ERRORS } from '@/src/firebase/errors'
+import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth'
 
 type LoginProps = {
 
@@ -14,6 +17,12 @@ const Login:React.FC<LoginProps> = () => {
     password:''
   })
 
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    userError
+  ] = useSignInWithEmailAndPassword(auth)
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setLoginForm(prev => ({
       ...prev,
@@ -22,7 +31,10 @@ const Login:React.FC<LoginProps> = () => {
   }
 
   // firebase logic
-  const handleSubmit = () => {}
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    signInWithEmailAndPassword(loginForm.email,loginForm.password)
+  }
   return (
     <form onSubmit={handleSubmit} >
       <Input required name='email' placeholder='email' type='email' mb={2} onChange={handleChange} fontSize='10pt' _placeholder={{
@@ -57,8 +69,12 @@ const Login:React.FC<LoginProps> = () => {
       }}
       mb={2}
        />
-      <Button mt={2} mb={2} width='100%' height='36px' >Login</Button>
-
+       <Text textAlign='center' color='red.600' >
+         {FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}
+       </Text>
+       
+      <Button isLoading={loading} type='submit' mt={2} mb={2} width='100%' height='36px' >Login</Button>
+      
       <Flex>
         <Text mr={2} >New Here</Text>
         <Text onClick={() => setAuthModalState((prev) => ({
