3f21da76278dcfbb4ddd401ab07914ee061655f3
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-06 08:47:02 +0100

    added a link to the homepage on the reddit logo

--- reddit-clone/src/components/navbar/Navbar.tsx
+++ reddit-clone/src/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {Directory, RightContent, SearchInput} from '../index'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import { auth } from '@/src/firebase/clientApp'
+import Link from 'next/link'
 
 
 const Navbar:React.FC = () => {
@@ -10,7 +11,10 @@ const Navbar:React.FC = () => {
   return (
     <Flex justifyContent={{md:'space-between'}} bg='white' height='44px' padding='6px 12px' >
       <Flex width={{base:'40px',md:'auto'}} align='center' mr={{base:0,md:2}} >
-        <Image src='/images/redditFace.svg' alt="" height={30} />
+        <Link href='/'>
+          <Image src='/images/redditFace.svg' alt="" height={30} />
+
+        </Link>
         <Image src='/images/redditText.svg' alt="" height={46} display={{
           base:'none',md:'unset'
         }}  />
