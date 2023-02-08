36b8c15aa883ceed4e21f99aaa285345115d08ad
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-08 09:49:41 +0100

    single post page

---
+++ reddit-clone/src/pages/r/[communityId]/comments/[pid].tsx
@@ -0,0 +1,107 @@
+import React, { useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Post } from "@/src/atoms/postsAtom";
+import { auth, firestore } from "@/src/firebase/clientApp";
+import useCommunityData from "@/src/hooks/useCommunityData";
+import usePosts from "@/src/hooks/usePosts";
+import {About,PageContent,Loader,Comments,PostItem} from '@/src/components'
+
+type PostPageProps = {};
+
+const PostPage: React.FC<PostPageProps> = () => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const { community, pid } = router.query;
+  const { communityStateValue } = useCommunityData();
+
+  // Need to pass community data here to see if current post [pid] has been voted on
+  const {
+    postStateValue,
+    setPostStateValue,
+    onDeletePost,
+    loading,
+    setLoading,
+    onVote,
+  } = usePosts(communityStateValue.currentCommunity);
+
+  const fetchPost = async () => {
+    console.log("FETCHING POST");
+
+    setLoading(true);
+    try {
+      const postDocRef = doc(firestore, "posts", pid as string);
+      const postDoc = await getDoc(postDocRef);
+      setPostStateValue((prev) => ({
+        ...prev,
+        selectedPost: { id: postDoc.id, ...postDoc.data() } as Post,
+      }));
+      // setPostStateValue((prev) => ({
+      //   ...prev,
+      //   selectedPost: {} as Post,
+      // }));
+    } catch (error: any) {
+      console.log("fetchPost error", error.message);
+    }
+    setLoading(false);
+  };
+
+  // Fetch post if not in already in state
+  useEffect(() => {
+    const { pid } = router.query;
+
+    if (pid && !postStateValue.selectedPost) {
+      fetchPost();
+    }
+  }, [router.query, postStateValue.selectedPost]);
+
+  return (
+    <PageContent>
+      {/* Left Content */}
+      <>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            {postStateValue.selectedPost && (
+              <>
+                <PostItem
+                  post={postStateValue.selectedPost}
+                  // postIdx={postStateValue.selectedPost.postIdx}
+                  onVote={onVote}
+                  onDeletePost={onDeletePost}
+                  userVoteValue={
+                    postStateValue.postVotes.find(
+                      (item) => item.postId === postStateValue.selectedPost!.id
+                    )?.voteValue
+                  }
+                  userIsCreator={
+                    user?.uid === postStateValue.selectedPost.creatorId
+                  }
+                  router={router}
+                />
+                <Comments
+                  user={user}
+                  community={community as string}
+                  selectedPost={postStateValue.selectedPost}
+                />
+              </>
+            )}
+          </>
+        )}
+      </>
+      {/* Right Content */}
+      <>
+        <About
+          communityData={
+            communityStateValue.currentCommunity
+            // communityStateValue.visitedCommunities[community as string]
+          }
+          loading={loading}
+        />
+      </>
+    </PageContent>
+  );
+};
+export default PostPage;
