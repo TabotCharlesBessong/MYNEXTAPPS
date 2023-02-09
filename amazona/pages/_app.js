0c7736f9c7abb0afe0c99a9dc1539403eecad232
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-09 13:29:06 +0100

    lets make a new builds

---
+++ amazona/pages/_app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import '../styles/global.scss';
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  );
+}
+
+export default MyApp;
---
+++ amazona/pages/index.js
@@ -0,0 +1,13 @@
+
+
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <h1 className="text-5xl text-orange-600 text-center">
+        Hello my world
+      </h1>
+    </div>
+  );
+}
+
+
