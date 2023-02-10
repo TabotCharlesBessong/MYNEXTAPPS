5e280b52fd66429a8d9ee2c22b0e8d80153e92f0
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-10 09:09:11 +0100

    seeding users to mongoDB database

---
+++ amazona/pages/api/seed.js
@@ -0,0 +1,14 @@
+
+import db from '../../utils/db'
+import User from "../../models/User";
+import data from "../../utils/data";
+
+const handler = async (req,res) => {
+  db.connect()
+  await User.deleteMany()
+  await User.insertMany(data.users);
+  await db.disconnect()
+  res.send({message:'seeded successfully'})
+}
+
+export default handler
