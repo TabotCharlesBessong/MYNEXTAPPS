1917855e963d0606f138101d366466287a55c119
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-10 11:43:16 +0100

    shipping page

--- amazona/utils/Store.js
+++ amazona/utils/Store.js
@@ -44,6 +44,28 @@ const reducer = (state,action) => {
 				},
 			};
 
+		case "CART_CLEAR_ITEMS":
+			return { ...state, cart: { ...state.cart, cartItems: [] } };
+
+		case "SAVE_SHIPPING_ADDRESS":
+			return {
+				...state,
+				cart: {
+					...state.cart,
+					shippingAddress: {
+						...state.cart.shippingAddress,
+						...action.payload,
+					},
+				},
+			};
+		case "SAVE_PAYMENT_METHOD":
+			return {
+				...state,
+				cart: {
+					...state.cart,
+					paymentMethod: action.payload,
+				},
+			};
 		default:
 			return state;
 	}
