c2c81ab09555a8dda28b3074b9d3e0897a614482
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-10 08:40:03 +0100

    login page with validation

--- amazona/pages/cart.js
+++ amazona/pages/cart.js
@@ -102,7 +102,7 @@ const CartScreen = () => {
 							</li>
 							<li>
 								<button
-									onClick={() => router.push("/shipping")}
+									onClick={() => router.push("login?redirect=/shipping")}
 									className="primary-button w-full"
 								>
 									Check Out
