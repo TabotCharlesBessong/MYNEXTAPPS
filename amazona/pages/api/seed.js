5813a9ba7215dc7c81b07b93cd7b2d9a8d70fd80
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-11 10:09:14 +0100

    seeding product to database

--- amazona/pages/api/seed.js
+++ amazona/pages/api/seed.js
@@ -1,12 +1,15 @@
 
 import db from '../../utils/db'
 import User from "../../models/User";
+import Product from '../../models/Product'
 import data from "../../utils/data";
 
 const handler = async (req,res) => {
   db.connect()
   await User.deleteMany()
   await User.insertMany(data.users);
+  await Product.deleteMany()
+  await Product.insertMany(data.products)
   await db.disconnect()
   res.send({message:'seeded successfully'})
 }
