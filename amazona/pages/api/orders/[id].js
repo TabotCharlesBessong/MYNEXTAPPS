ba1ddca8b33743103f4a4f5badcf5e876fecffb1
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-11 17:28:24 +0100

    order details

---
+++ amazona/pages/api/orders/[id].js
@@ -0,0 +1,19 @@
+
+import {getSession} from 'next-auth/react'
+import db from '../../../utils/db'
+import Order from '../../../models/db'
+
+const handler = async (req,res) => {
+  const session = await getSession({req})
+  if(!session) {
+    return res.status(401).send('sign in required')
+  }
+
+  await db.connect()
+
+  const order = await Order.findById(req.query.id)
+  await db.disconnect()
+  res.send(order)
+}
+
+export default handler
