87e26411d5130f50a19a7ed0b502d9bd0bd99fd2
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-14 12:07:04 +0100

    redirect

--- amazona/pages/login.js
+++ amazona/pages/login.js
@@ -93,7 +93,7 @@ const LoginScreen = () =>{
 				</div>
 				<div className="mb-4 ">
 					Don&apos;t have an account? &nbsp;
-					<Link href="/register">Register</Link>
+					<Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
 				</div>
 			</form>
 		</Layout>
