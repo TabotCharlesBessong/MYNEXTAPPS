ed2625cbe52b57fd7de3900c1bdd8fac5eb58a6d
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-16 11:17:33 +0100

    login component

--- firebase-chat-app/src/pages/_app.js
+++ firebase-chat-app/src/pages/_app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../styles/globals.css'
 import {Login} from '../components'
+import {ChakraProvider} from '@chakra-ui/react'
  
 function MyApp({ Component,pageProps }) {
   return (
     // <Component {...pageProps} />
-    <Login/>
+    <ChakraProvider>
+      <Login/>
+
+    </ChakraProvider>
   );
 }
 
