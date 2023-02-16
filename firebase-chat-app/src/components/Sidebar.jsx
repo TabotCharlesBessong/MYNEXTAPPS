59bb2e0da4d995b41d28b9841db8aa8bd06dea43
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-16 13:02:08 +0100

    sidebar

---
+++ firebase-chat-app/src/components/Sidebar.jsx
@@ -0,0 +1,75 @@
+import { Button,Avatar ,IconButton,Flex,Text} from "@chakra-ui/react";
+import { ArrowLeftIcon } from "@chakra-ui/icons";
+// import { auth, db } from "../firebase";
+
+const Chat = () => {
+  return (
+		<Flex
+			// justifyContent="space-between"
+			align="center"
+			_hover={{ cursor: "pointer", bg: "gray.100" }}
+			p={3}
+		>
+			<Avatar marginEnd={3} src="" />
+			<Text>Hello men how are you doing</Text>
+		</Flex>
+	);
+}
+
+export default function Sidebar() {
+
+  return (
+		<Flex
+			// bg="blue.100"
+			h="100%"
+			w="300px"
+			borderEnd="1px solid"
+			borderColor="gray.200"
+			direction="column"
+		>
+			<Flex
+				// bg="red.100"
+				h="81px"
+				w="100%"
+				align="center"
+				justifyContent="space-between"
+				borderBottom="1px solid"
+				borderColor="gray.200"
+				p={3}
+        position='sticky'
+			>
+				<Flex align="center">
+					<Avatar src="" marginEnd={3} />
+					<Text>Charles</Text>
+				</Flex>
+
+				<IconButton size="sm" isRound icon={<ArrowLeftIcon />} onClick={{}} />
+			</Flex>
+
+			<Button m={5} p={4} onClick={{}}>
+				New Chat
+			</Button>
+			<Flex
+        overflowX='scroll'
+				direction="column"
+				sx={{ scrollbarWidth: "none" }}
+				flex={1}
+			>
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+				<Chat />
+			</Flex>
+		</Flex>
+	);
+}
--- firebase-chat-app/src/pages/_app.js
+++ firebase-chat-app/src/pages/_app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../styles/globals.css'
-import {Login} from '../components'
+import {Login, Sidebar} from '../components'
 import {ChakraProvider} from '@chakra-ui/react'
  
 function MyApp({ Component,pageProps }) {
   return (
     // <Component {...pageProps} />
     <ChakraProvider>
-      <Login/>
+      {/* <Login/> */}
+      <Sidebar/>
 
     </ChakraProvider>
   );
