401a5c0269c812738675c53cd913a47d7fa01e29
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-05-02 17:04:30 +0100

    setting up prisma

---
+++ todo_app/prisma/migrations/20230502154955_post/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "content" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
