ec6769100a0f974bccfdb63383cceae96e042c7f
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-06-18 20:43:30 +0100

    created prisma model and configuration

--- airbnb-clone/app/layout.tsx
+++ airbnb-clone/app/layout.tsx
@@ -3,7 +3,9 @@ import { ClientOnly, Navbar, RegisterModal } from './components'
 import './globals.css'
 import ToasterProvider from './providers/ToasterProvider'
 
-const nunito = Nunito({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })   
+
+
 
 export const metadata = {
   title: 'AirBnB Clone',
