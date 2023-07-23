004fd2cd38d015e7203acdb514a0cf50f5ea8cd4
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-07-23 16:55:36 +0100

    feat:(category for navigation and property types)

--- airbnb-clone/app/components/navbar/Navbar.tsx
+++ airbnb-clone/app/components/navbar/Navbar.tsx
@@ -1,33 +1,29 @@
-
-'use client'
+"use client";
 
 import React from "react";
-import {Container,Logo, Search,UserMenu} from '../index'
+import { Categories, Container, Logo, Search, UserMenu } from "../index";
 import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
-  currentUser?: SafeUser | null
+  currentUser?: SafeUser | null;
 }
 
-const Navbar: React.FC<NavbarProps> = ({
-  currentUser
-}) => {
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   // console.log({currentUser})
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm" >
+    <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
-
+        <Container>
+          <div className="flex flex-grow items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
       </div>
-      <Container>
-        <div className="flex flex-grow items-center justify-between gap-3 md:gap-0">
-
-        </div>
-        <Logo/>
-        <Search/>
-        <UserMenu currentUser={currentUser} />
-      </Container>
+      <Categories/>
     </div>
-  )
+  );
 };
 
 export default Navbar;
