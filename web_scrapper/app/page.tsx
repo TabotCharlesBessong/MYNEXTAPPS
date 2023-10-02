0cb1aba554994ffc42067940c3ca3e7e7dc6f3ac
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-02 18:46:25 +0100

    home page and added searchbar and carousel

--- web_scrapper/app/page.tsx
+++ web_scrapper/app/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { HeroCarousel, Searchbar } from "@/components";
+import Image from "next/image";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping start here
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                width={16}
+                height={16}
+                alt="arrow-right"
+              />
+            </p>
+            <h1 className="head-text">
+              Unleash the power of
+              <span className="text-primary"> Pricewise</span>
+            </h1>
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+            <Searchbar />
+          </div>
+          <HeroCarousel />
+        </div> 
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {
+            ['Apple iphone','Book','Sneakers'].map(product => (
+              <div>
+                {product}
+              </div>
+            ))
+          }
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default page
+export default page;
