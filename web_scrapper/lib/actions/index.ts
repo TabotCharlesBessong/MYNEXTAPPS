18988932041a2abb827f6f9d2e8818ca2f803a79
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-04 21:50:46 +0100

    database setup

--- web_scrapper/lib/actions/index.ts
+++ web_scrapper/lib/actions/index.ts
@@ -1,13 +1,19 @@
 
 "use server"
 
+import { connectToDB } from "../mongoose"
 import { scrapeAmazonProduct } from "../scrapper"
 
 export const scrapeAndStoreProduct = async (productUrl:string) => {
   if(!productUrl) return
 
   try {
+    connectToDB()
     const scrappedProduct = await scrapeAmazonProduct(productUrl)
+
+    if(!scrappedProduct) return
+
+
   } catch (error:any) {
     // console.log(error)
     throw new Error(`Failed to create/update product: ${error.message}`)
