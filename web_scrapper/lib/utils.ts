5e4ada767562e17482579394eb0584a8ed74b779
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-04 20:29:51 +0100

    getting the data from the scrapped informartion

---
+++ web_scrapper/lib/utils.ts
@@ -0,0 +1,48 @@
+export function extractPrice(...elements: any) {
+  for (const element of elements) {
+    const priceText = element.text().trim();
+
+    if (priceText) {
+      const cleanPrice = priceText.replace(/[^\d.]/g, "");
+
+      let firstPrice;
+
+      if (cleanPrice) {
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      }
+
+      return firstPrice || cleanPrice;
+    }
+  }
+
+  return "";
+}
+
+export function extractCurrency(element: any) {
+  const currencyText = element.text().trim().slice(0, 1);
+  return currencyText ? currencyText : "";
+}
+
+// Extracts description from two possible elements from amazon
+export function extractDescription($: any) {
+  // these are possible elements holding description of the product
+  const selectors = [
+    ".a-unordered-list .a-list-item",
+    ".a-expander-content p",
+    // Add more selectors here if needed
+  ];
+
+  for (const selector of selectors) {
+    const elements = $(selector);
+    if (elements.length > 0) {
+      const textContent = elements
+        .map((_: any, element: any) => $(element).text().trim())
+        .get()
+        .join("\n");
+      return textContent;
+    }
+  }
+
+  // If no matching elements were found, return an empty string
+  return "";
+}
