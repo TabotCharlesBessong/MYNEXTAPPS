95dba0531241ed9e8c9d277ad75463f3083e797e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-05 14:23:56 +0100

    storing the product in the database

--- web_scrapper/lib/actions/index.ts
+++ web_scrapper/lib/actions/index.ts
@@ -1,8 +1,11 @@
 
 "use server"
 
+import Product from "../model/product.model"
 import { connectToDB } from "../mongoose"
 import { scrapeAmazonProduct } from "../scrapper"
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils"
+import { revalidatePath } from "next/cache"
 
 export const scrapeAndStoreProduct = async (productUrl:string) => {
   if(!productUrl) return
@@ -13,6 +16,34 @@ export const scrapeAndStoreProduct = async (productUrl:string) => {
 
     if(!scrappedProduct) return
 
+    let product = scrappedProduct
+
+    const existingProduct = await Product.findOne({url:scrappedProduct.url})
+
+    if(existingProduct){
+      const updatedPriceHistory : any = [
+        ...existingProduct.priceHistory,
+        {price:scrappedProduct.currentPrice}
+      ]
+
+      product = {
+        ...scrappedProduct,
+        priceHistory:updatedPriceHistory,
+        lowestPrice:getLowestPrice(updatedPriceHistory),
+        highestPrice:getHighestPrice(updatedPriceHistory),
+        averagePrice:getAveragePrice(updatedPriceHistory)
+      }
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      {url:scrappedProduct.url},
+      product,
+      {
+        upsert:true,new:true
+      }
+    )
+
+    revalidatePath(`/products/${newProduct._id}`)
 
   } catch (error:any) {
     // console.log(error)
