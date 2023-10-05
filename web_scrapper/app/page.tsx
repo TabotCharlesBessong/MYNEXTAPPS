b0a432d9272feaa4f0064bb0710dbdb4fdffc90a
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-05 16:13:25 +0100

    displaying all product

--- web_scrapper/app/page.tsx
+++ web_scrapper/app/page.tsx
@@ -1,8 +1,10 @@
-import { HeroCarousel, Searchbar } from "@/components";
+import { HeroCarousel, ProductCard, Searchbar } from "@/components";
+import { getAllProducts } from "@/lib/actions";
 import Image from "next/image";
 import React from "react";
 
-const page = () => {
+const page = async ()  => {
+  const allProducts = await getAllProducts()
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -28,19 +30,18 @@ const page = () => {
             <Searchbar />
           </div>
           <HeroCarousel />
-        </div> 
+        </div>
       </section>
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {
-            ['Apple iphone','Book','Sneakers'].map(product => (
-              <div>
-                {product}
-              </div>
-            ))
-          }
-        </div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+          {/* {allProducts?.map((product:any) => (
+            <div>{product.title}</div>
+          ))} */}
+        </div> 
       </section>
     </>
   );
--- web_scrapper/components/index.ts
+++ web_scrapper/components/index.ts
@@ -2,3 +2,4 @@
 export {default as Navbar} from './Navbar'
 export {default as Searchbar} from './Searchbar'
 export {default as HeroCarousel} from './HeroCarousel'
+export {default as ProductCard} from './ProductCard'
