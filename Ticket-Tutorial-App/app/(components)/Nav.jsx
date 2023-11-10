34d351f6e5bdf78fcf08110026e34000a8674104
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-10 06:18:50 +0100

    email

--- Ticket-Tutorial-App/app/(components)/Nav.jsx
+++ Ticket-Tutorial-App/app/(components)/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
         </Link>
       </div>
       <div>
-        <p className=" text-default-text">jake.lower17@gmail.com</p>
+        <p className=" text-default-text">charlestabot@gmail.com</p>
       </div>
     </nav>
   );
