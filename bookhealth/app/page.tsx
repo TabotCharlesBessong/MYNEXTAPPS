3311427ab4eef4de7a1c8a8b2cea7ed00829e4ab
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-07-15 11:05:10 +0100

    lets try the web version

---
+++ bookhealth/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-1 items-center justify-center" >
+      <h1 className="text-3xl text-orange-600 text-center font-bold">
+        Hello
+      </h1>
+    </div>
+  );
+}
