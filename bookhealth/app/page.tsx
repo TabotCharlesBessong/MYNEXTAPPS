b083ec521775f72cda1b5df16c7c0bdf75a3f3e3
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-07-15 12:17:21 +0100

    setting up the homepage and adding resources

--- bookhealth/app/page.tsx
+++ bookhealth/app/page.tsx
@@ -1,13 +1,36 @@
-import { Button } from "@/components/ui/button";
+// import { PatientForm } from "@/components";
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 items-center justify-center" >
-      <h1 className="text-3xl text-white text-center font-bold">
-        Hello
-      </h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            width={1000}
+            height={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2024 Kido Medical
+            </p>
+            <Link href="/?admin=true">Admin</Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        width={1000}
+        height={1000}
+        alt="patient"
+        className="size-img max-w-[50%]"
+      />
     </div>
   );
 }
