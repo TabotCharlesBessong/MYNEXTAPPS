fa6152a863c171d3d795cf5ea48e961ca00404cf
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-07-15 11:53:12 +0100

    setting up the application

--- bookhealth/app/page.tsx
+++ bookhealth/app/page.tsx
@@ -1,11 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-1 items-center justify-center" >
-      <h1 className="text-3xl text-orange-600 text-center font-bold">
+      <h1 className="text-3xl text-white text-center font-bold">
         Hello
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
