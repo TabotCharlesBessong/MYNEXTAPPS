ddcc98ebb08b93a04e97aef6baf9a80ccea75340
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-09 04:26:25 +0100

    setting up the project and started building the sidebar

---
+++ scrape_flow/app/[dashboard]/[home]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='bg-red-600 flex-1 h-screen mt-4 pt-6 rounded-xl' >
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage
