d81d0f2d716e4e752685f620ffea64555692ac3c
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-29 21:37:35 +0100

    setting up supabase types

--- gadget_shop_admin/src/supabase/server.ts
+++ gadget_shop_admin/src/supabase/server.ts
@@ -1,10 +1,11 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { Database } from "./types";
 
 export async function createClient() {
   const cookieStore = await cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
