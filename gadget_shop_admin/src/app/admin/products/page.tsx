c4bb1e6f32b9f93d162f01affdc66966cdd4ee08
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-01-01 06:15:17 +0100

    completed projduct page, component, actions and schema

--- gadget_shop_admin/src/app/admin/products/page.tsx
+++ gadget_shop_admin/src/app/admin/products/page.tsx
@@ -1,11 +1,16 @@
-"use client"
+import { getCategoriesWithProducts } from "@/actions/categories";
+import { getProductsWithCategories } from "@/actions/products";
+import { ProductPageComponent } from "./page-component";
 
-import React from 'react'
 
-const page = () => {
+export default async function Products() {
+  const categories = await getCategoriesWithProducts();
+  const productsWithCategories = await getProductsWithCategories();
+
   return (
-    <div>page</div>
-  )
+    <ProductPageComponent
+      categories={categories}
+      productsWithCategories={productsWithCategories}
+    />
+  );
 }
-
-export default page
