2fa0538c30826d231be2c5339e1f070f20afd32c
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-03-22 22:56:49 +0100

    created the favourites page

--- car_dealership_cms/src/app/presentation/inventory/layout.tsx
+++ car_dealership_cms/src/app/presentation/inventory/layout.tsx
@@ -1,6 +1,5 @@
-import { PublicLayout } from "@/components/layouts/public-layout";
 import type { PropsWithChildren } from "react";
 
 export default function PresentationLayout(props: PropsWithChildren) {
-  return <PublicLayout>{props.children}</PublicLayout>;
+  return <div>{props.children}</div>;
 }
