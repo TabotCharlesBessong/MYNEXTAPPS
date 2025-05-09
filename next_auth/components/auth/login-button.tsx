4a087e3eed514d869980041b76d2f022fd992e80
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-09 15:59:43 +0100

    created the homepage

---
+++ next_auth/components/auth/login-button.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+interface LoginButtonProps {
+  children: React.ReactNode
+  mode?: "modal" | "redirect"
+  asChild?: boolean
+}
+
+const LoginButton = ({children,mode="redirect",asChild}:LoginButtonProps) => {
+
+  const onClick = () => {
+    const router = useRouter()
+    console.log("clicked the login button");
+    router.push("/auth/login")
+  }
+  return (
+    <span className='cursor-pointer' onClick={onClick} >
+      {children}
+    </span>
+  )
+}
+
+export default LoginButton
