6a5434abc57f7506cf1dcf3a54dda74f3c7f3f43
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-09 15:18:28 +0100

    bootstraping the project

---
+++ next_auth/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p>Hello Auth</p>
+      <Button variant="secondary" >Click me</Button>
+    </div>
+  );
+}
