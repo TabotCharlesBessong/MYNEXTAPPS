01dda7c387e3f7f8e174b36caea3034c97190886
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-08-14 18:13:18 +0100

    .....................

--- caching/app/utils/api-helpers.js
+++ caching/app/utils/api-helpers.js
@@ -1,24 +1,24 @@
 export const getData = async (apiURL, caller, options = {}) => {
-    const url = new URL(apiURL);
+  const url = new URL(apiURL);
 
-    console.log(`[${caller}]: fetching ${url.pathname} started`);
+  console.log(`[${caller}]: fetching ${url.pathname} started`);
 
-    const startTime = performance.now();
+  const startTime = performance.now();
 
-    const response = await fetch(apiURL, options);
+  const response = await fetch(apiURL, options);
 
-    const endTime = performance.now();
-    const duration = (endTime - startTime).toFixed(2);
+  const endTime = performance.now();
+  const duration = (endTime - startTime).toFixed(2);
 
-    if (!response.ok) {
-        console.error(`fetch url ${url.pathname} failed`);
-        throw new Error(`[${caller}]: failed to fetch ${url.pathname}`);
-    }
+  if (!response.ok) {
+    console.error(`fetch url ${url.pathname} failed`);
+    throw new Error(`[${caller}]: failed to fetch ${url.pathname}`);
+  }
 
-    const data = await response.json();
-    console.log(
-        `[${caller}]: fetching ${url.pathname} completed in ${duration}ms`
-    );
+  const data = await response.json();
+  console.log(
+    `[${caller}]: fetching ${url.pathname} completed in ${duration}ms`
+  );
 
-    return data;
+  return data;
 };
